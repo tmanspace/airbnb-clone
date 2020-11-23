802ec73b69b332895bbe8925d7d31b307b0e30dd
Tim <serazutdinov.tim@gmail.com>
2020-11-23 23:58:40 +0300

    Users were seeded

--- users/management/commands/seed_users.py
+++ users/management/commands/seed_users.py
@@ -1,8 +1,20 @@
 from django.core.management.base import BaseCommand
-
-# from users import models
+from django_seed import Seed
+from users.models import User
 
 
 class Command(BaseCommand):
+
+    help = "This command adds users how much you wish"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--number", default=2, type=int, help="How many users to create?"
+        )
+
     def handle(self, *args, **options):
-        pass
+        number = options.get("number")
+        seeder = Seed.seeder()
+        seeder.add_entity(User, number, {"is_staff": False, "is_superuser": False})
+        seeder.execute()
+        self.stdout.write(self.style.SUCCESS(f"{number} users were created!"))
