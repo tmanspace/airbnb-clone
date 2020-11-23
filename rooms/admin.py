efd9d0ae3833f832017686479925a2042d08b481
Tim <serazutdinov.tim@gmail.com>
2020-11-24 01:21:46 +0300

    Rooms seed done

--- rooms/admin.py
+++ rooms/admin.py
@@ -96,5 +96,9 @@ class RoomAdmin(admin.ModelAdmin):
     def count_amenities(self, obj):
         return obj.amenities.count()
 
+    count_amenities.short_description = "Amenities Count"
+
     def count_photos(self, obj):
         return obj.photos.count()
+
+    count_photos.short_description = "Photo Count"
--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -1,5 +1,6 @@
 import random
 from django.core.management.base import BaseCommand
+from django.contrib.admin.utils import flatten
 from django_seed import Seed
 from users.models import User
 from rooms import models as room_models
@@ -34,5 +35,29 @@ class Command(BaseCommand):
                 "baths": lambda x: random.randint(1, 5),
             },
         )
-        seeder.execute()
+        created_rooms = seeder.execute()
+        created_clean = flatten(list(created_rooms.values()))
+        amenities = room_models.Amenity.objects.all()
+        facilities = room_models.Facility.objects.all()
+        rules = room_models.HouseRule.objects.all()
+        for pk in created_clean:
+            room = room_models.Room.objects.get(pk=pk)
+            for i in range(3, random.randint(7, 10)):
+                room_models.Photo.objects.create(
+                    caption=seeder.faker.sentence(),
+                    room=room,
+                    file=f"/room_photos/{random.randint(1,31)}.webp",
+                )
+            for a in amenities:
+                m_n = random.randint(0, 15)
+                if m_n % 2 == 0:
+                    room.amenities.add(a)
+            for f in facilities:
+                m_n = random.randint(0, 15)
+                if m_n % 2 == 0:
+                    room.facilities.add(f)
+            for r in rules:
+                m_n = random.randint(0, 15)
+                if m_n % 2 == 0:
+                    room.house_rules.add(r)
         self.stdout.write(self.style.SUCCESS(f"{number} rooms were created!"))
