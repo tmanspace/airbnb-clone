6981a76b289e7501a8444222e3ec92dac9359a75
Tim <serazutdinov.tim@gmail.com>
2020-11-23 23:39:44 +0300

    Revert "Amenities seeded"
    This reverts commit b24b6a8df723d0755a62cecf191516db5ff0b650.

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,7 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-THIRD_PARTY_APPS = ["django_countries", "django_seed"]
+THIRD_PARTY_APPS = ["django_countries"]
 
 PROJECT_APPS = [
     "core.apps.CoreConfig",
