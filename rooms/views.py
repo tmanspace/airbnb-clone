a8d6d1112e0fd2324a6a6c368b4801dbe2e1469d
Tim <serazutdinov.tim@gmail.com>
2020-11-30 02:02:25 +0300

    Pagination with paginator

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
+from . import models
 
 # Create your views here.
 
 
 def all_rooms(req):
-    return render(req, template_name="all_rooms.html")
+    page = req.GET.get("page", 1)
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10, orphans=5)
+
+    try:
+        rooms = paginator.page(int(page))
+        return render(
+            req,
+            template_name="rooms/home.html",
+            context={"page": rooms},
+        )
+    except EmptyPage:
+        return redirect("/")
