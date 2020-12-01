6f66633810a381c1d6b7da552e029b60b7fb64b5
Tim <serazutdinov.tim@gmail.com>
2020-12-01 23:08:30 +0300

    Detail view changed

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,7 @@
 from django.utils import timezone
+from django.urls import reverse
 from django.views.generic import ListView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models
 
 # Create your views here.
@@ -21,7 +22,11 @@ class HomeView(ListView):
 
 
 def room_detail(request, pk):
-    return render(request, "rooms/detail.html")
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        return redirect(reverse("core:home"))
 
 
 """ PAGINATOR """
