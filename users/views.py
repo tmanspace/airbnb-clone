87c685a95935c823fb58835a9218b1920c964d41
Tim <serazutdinov.tim@gmail.com>
2020-12-10 23:51:07 +0300

    Login page initial

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+
+class LoginView(View):
+
+    def get(self, req):
+        return render(req, "users/login.html")
+
+    def post(self, req):
+        pass
