189697bc5d6223107bf966e3d0d1b9cbc00e2e78
Tim <serazutdinov.tim@gmail.com>
2020-12-15 23:43:37 +0300

    Sign up 1

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,45 @@
 from django import forms
+from . import models
+
 
 class LoginForm(forms.Form):
 
     email = forms.EmailField()
-    password = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        try:
+            user = models.User.objects.get(email=email)
+            if user.check_password(password):
+                return self.cleaned_data
+            else:
+                self.add_error("password", forms.ValidationError("Password is wrong"))
+        except models.User.DoesNotExist :
+            self.add_error("email", forms.ValidationError("User does not exist"))
+
+
+class SignUpForm(forms.Form):
+
+    first_name = forms.CharField(max_length=80)
+    last_name = forms.CharField(max_length=80)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput, label="Confirm password")
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(email=email)
+            raise forms.ValidationError("User with that email already exists")
+        except models.User.DoesNotExist:
+            return email
+
+    def clean_password2(self):
+        password = self.cleaned_data.get("password")
+        password2 = self.cleaned_data.get("password2")
+
+        if password != password2:
+            raise forms.ValidationError("Passwords are not the same")
+        return password
--- users/views.py
+++ users/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.urls import reverse_lazy
 from django.views import View
+from django.views.generic import FormView
+from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 # Create your views here.
@@ -8,8 +11,37 @@ from . import forms
 class LoginView(View):
 
     def get(self, req):
-        form = forms.LoginForm()
+        if (req.user.username != ""):
+            return redirect(reverse("core:home"))
+        form = forms.LoginForm(initial={"email": "serazutdinov.tim@gmail.com"})
         return render(req, "users/login.html", context={"form" : form})
 
     def post(self, req):
-        pass
+        form = forms.LoginForm(req.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            user = authenticate(req, username=email, password=password)
+            if user is not None:
+                login(req, user)
+                return redirect(reverse("core:home"))
+        return render(req, "users/login.html", {"form": form})
+
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+    initial = {
+        "first_name": "Tim",
+        "last_name": "Ser",
+        "email": "hello@gmail.com",
+        "password": "nice12Fg#",
+        "password2": "nice12Fg#"
+    }
